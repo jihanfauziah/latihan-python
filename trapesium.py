f6d8059abe787464f22a343fd31531a195e45d8c
jihanfauziah <zihanfaujiah1616@gmail.com>
2024-10-22 08:08:27 +0700

    Update trapesium.py

--- trapesium.py
+++ trapesium.py
@@ -1,5 +1,18 @@
-print("="*40)
-print("TRAPESIUM")
-print("="*40)
+print('='*40)
+print('             PROGRAM TRAPESIUM')
+print('='*40)
 
-sisi_atas = float(input("masukan sisi atas trapesium"))
+def trapesium():
+    a = float(input('Masukkan sisi a : '))
+    b = float(input('Masukkan sisi b : '))
+    c = float(input('Masukkan sisi c : '))
+    d = float(input('Masukkan sisi d : '))
+    t = float(input('Masukkan Tinggi : '))
+    l = lambda a,b,t: 1/2 * (a+b) * t
+    k = lambda a,b,c,d: a+b+c+d
+    print('LUAS : ',l(a,b,t),'cm')
+    print('KELILING : ',k(a,b,c,d,'cm'))
+
+trapesium()
+trapesium()
+trapesium()
