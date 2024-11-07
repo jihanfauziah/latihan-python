1e7f1bb30bb0f7e0bd7a5ad498fd46de035133d7
jihanfauziah <zihanfaujiah1616@gmail.com>
2024-11-07 20:04:10 +0700

    Add files via upload

---
+++ gaya.py
@@ -0,0 +1,8 @@
+print("Menghitung Gaya Fisika")
+
+m = int(input("Masuka massa : "))
+a = int(input("Masukan percepatan: "))
+
+f = m*a
+
+print(f'Massa : {m} kg/npercepatan : {a m\snGaya yang dihasilkan adalah {F}} N')
---
+++ perulangan.py
@@ -0,0 +1,4 @@
+#perulangan dalam python
+kata = 'I LOVE Jeon Wonwoo'
+for i in range (20) :
+    print (kata)    
